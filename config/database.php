63c62dc57235dc4fe6b77d3f5d4b25e779a5a2f7
adityadhanraj12 <adityadhanraj404@gmail.com>
2026-07-03 02:04:33 +0530

    Configure production database to use ca.pem and SSL verification

--- config/database.php
+++ config/database.php
@@ -58,8 +58,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ?? env('DB_SSL_CA'),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ?? base_path('ca.pem'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', true),
             ], function ($value) {
                 return $value !== null;
             }) : [],
