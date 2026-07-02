1a1f5e10dc1bf18f0092db31164f715eab846a51
adityadhanraj12 <adityadhanraj404@gmail.com>
2026-07-03 01:31:28 +0530

    Update MySQL config to support SSL verification configuration

--- config/database.php
+++ config/database.php
@@ -58,8 +58,11 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ?? env('DB_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT'),
+            ], function ($value) {
+                return $value !== null;
+            }) : [],
         ],
 
         'mariadb' => [
